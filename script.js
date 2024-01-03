c6f2df661edae1c821117c9a7fd57c516323d572
abenezer <54418157+Abenezer-Daniel-W@users.noreply.github.com>
2024-01-03 10:32:37 -0700

    added classes

--- script.js
+++ script.js
@@ -23,12 +23,13 @@ closeForm.addEventListener("click", function() {
 });
 
 
-function Book(title, author, page, status) {
-    // the constructor...
-    this.title = title;
-    this.author = author;
-    this.page = page;
-    this.status = status;
+class Book{
+	constructor(title, author, page, status) {
+		this.title = title;
+		this.author = author;
+		this.page = page;
+		this.status = status;
+}
 }
 
 function addBookToLibrary() {
